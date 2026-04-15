7f5a2f946331af4c6d0a34408c8e03cbde0af7f0
k-iwamoto <k-iwamoto@CD26302L.arksystems.co.jp>
2026-04-15 09:58:13 +0900

    lesson-01-challenge08完了

--- Java100_questions_cac2nd/src/lesson01/challenge08/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge08/Patisserie.java
@@ -107,5 +107,6 @@ public class Patisserie {
 		System.out.println("シトロン      \\" + price_sitron + "・・・ " + ratio_sitron + "%");
 		System.out.println("ショコラ      \\" + price_chocolate + "・・・ " + ratio_chocolate + "%");
 		System.out.println("ピスターシュ  \\" + price_pistach + "・・・ " + ratio_pistach + "%");
+		//テスト
 	}
 }
