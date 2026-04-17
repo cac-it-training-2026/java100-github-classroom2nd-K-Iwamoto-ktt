92fd3ab9b8e30d64ff2812ccc92fd33a061f3fe6
k-iwamoto <k-iwamoto@CD26302L.arksystems.co.jp>
2026-04-17 17:03:47 +0900

    lesson02完了

--- Java100_questions_cac2nd/src/lesson02/challenge09/Detective.java
+++ Java100_questions_cac2nd/src/lesson02/challenge09/Detective.java
@@ -66,7 +66,13 @@ public class Detective {
 		int height = Integer.parseInt(heightStr);
 		int weight = Integer.parseInt(weightStr);
 
-		//ここに記述
+		if ((185 < height) && ((60 <= weight) && (weight < 80))) {
+			System.out.println("\n探偵：");
+			System.out.println("その方は犯人である可能性がありますね。");
+		} else {
+			System.out.println("\n探偵：");
+			System.out.println("その方は犯人ではありませんね。");
+		}
 
 	}
 
